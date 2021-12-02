890c880bfadf2361a34ce252eb34ffe76905acab
AlejandraCL <61206908+AlejandraCL@users.noreply.github.com>
2021-12-02 00:01:22 -0600

    add files(:

--- api.php
+++ api.php
@@ -1,35 +1,24 @@
 <?php
-header('Access-Control-Allow-Origin: *'); 
-switch ($_SERVER['REQUEST_METHOD']) {
-        
-  case 'GET': 
 
-     function ConseguirIp() {
-       if(isset($_SERVER['HTTP_CLIENT_IP'])) {
-          return $_SERVER['HTTP_CLIENT_IP'];
-       }
-       elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-          return $_SERVER['HTTP_X_FORWARDED_FOR'];
-       }
-       else {
-          return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
-      }
-     }
 
-     $IpApi = ConseguirIp(); 
-     $InfoApi = @unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' .$IpApi)); 
-  
-     $DatoPais = $InfoApi['geoplugin_countryName'];
-     $DatoCiudad    = $InfoApi['geoplugin_city'];
-    
+function getRealIP() {
 
-     $DatosUsuario = array(
-       "IP" => $IpApi,
-       "Pais" => $DatoPais,
-       "Ciudad" => $DatoCiudad
-     );
-     echo json_encode($DatosUsuario);     
-break;
-   default: 
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))
+            return $_SERVER['HTTP_CLIENT_IP'];
+           
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+       
+        return $_SERVER['REMOTE_ADDR'];
 }
-?>
+
+
+$geo = (unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.getRealIP())));
+$country = $geo["geoplugin_countryName"];
+$service = $geo["geoplugin_continentName"];
+
+$data = array();
+$data['Ip'] = getRealIP();
+$data['Ciudad'] = $country;
+$data['Servidor'] = $service;
+echo json_encode($data);
